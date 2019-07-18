ccff57792b099b78ee617b6947b8458c1df0e7d4
rucha2390 <ruchap@comitydesigns.com>
2019-07-18 15:06:51 +0530

    modified install.sh

--- build/install_sfdx.sh
+++ build/install_sfdx.sh
@@ -1,6 +1,3 @@
-#!/bin/zsh
-export SFDX_USE_GENERIC_UNIX_KEYCHAIN=true
-export SFDX_DOMAIN_RETRY=300
-npm install sfdx-cli
-node_modules/sfdx-cli/bin/run --version
-node_modules/sfdx-cli/bin/run plugins --core
+#Install dependencies, SFDX CLI in this case
+echo "Installing Dependencies... "
+sudo npm install -global sfdx-cli
